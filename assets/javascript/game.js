1fd2c2bb7da03ee29aaf2e3de5863a517738f932
Ipek Inan <kepinani1@gmail.com>
2018-10-07 11:27:52 -0400

    undated

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  var Random = Math.floor(Math.random() * 120) + 18;
+  //selects random number at start of game between 18 and 120
+  $("#randomNumber").text(Random);
+  var crystal1 = Math.floor(Math.random() * 12) + 1;
+  var crystal2 = Math.floor(Math.random() * 12) + 1;
+  var crystal3 = Math.floor(Math.random() * 12) + 1;
+  var crystal4 = Math.floor(Math.random() * 12) + 1;
+  $("#crystal1").text(crystal1);
+  $("#crystal2").text(crystal2);
+  $("#crystal3").text(crystal3);
+  $("#crystal4").text(crystal4);
+  //generates random number between 1 and 12
+  var myTotal = 0;
+  var wins = 0;
+  var losses = 0;
+  //sets users total, wins, and losses to 0 at start of game
+  $("#numberWins").text(wins);
+  $("#numberLosses").text(losses);
+  //declared variables
+  function reset() {
+    var Random = Math.floor(Math.random() * 120) + 18;
+    $("#randomNumber").text(Random);
+    var crystal1 = Math.floor(Math.random() * 12) + 1;
+    var crystal2 = Math.floor(Math.random() * 12) + 1;
+    var crystal3 = Math.floor(Math.random() * 12) + 1;
+    var crystal4 = Math.floor(Math.random() * 12) + 1;
+    $("#crystal1").text(crystal1);
+    $("#crystal2").text(crystal2);
+    $("#crystal3").text(crystal3);
+    $("#crystal4").text(crystal4);
+    var myTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
+    $("#finalTotal").text(myTotal);
+  }
+  function winnersCorner() {
+    wins++;
+    alert("Yeah, you won!");
+    $("#numberWins").text(wins);
+  }
+  function losersCorner() {
+    losses++;
+    alert("Bummer, you lost!");
+    $("#numberLosses").text(losses);
+  }
+  function addCrystalValue(text){
+    myTotal += text;
+    $("#finalTotal").text(myTotal);
+}
+  $("#crystal1").on("click", function () {
+    addCrystalValue(crystal1);
+    if (myTotal === Random) {
+      winnersCorner();
+    } else if (myTotal != Random) {
+      losersCorner();
+    }
+  })
+  $("#crystal2").on("click", function () {
+    addCrystalValue(crystal2);
+    if (myTotal === Random) {
+      winnersCorner();
+    } else if (myTotal != Random) {
+      losersCorner();
+    }
+  })
+  $("#crystal3").on("click", function () {
+    addCrystalValue(crystal3);
+    if (myTotal === Random) {
+      winnersCorner();
+    } else if (myTotal != Random) {
+      losersCorner();
+    }
+  })
+  $("#crystal4").on("click", function () {
+    addCrystalValue(crystal4);
+    if (myTotal === Random) {
+      winnersCorner();
+    } else if (myTotal != Random) {
+      losersCorner();
+    }
+  });
+});
